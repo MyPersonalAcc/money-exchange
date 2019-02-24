39ae75a35124e68b698f4820322f521d8f21ac23
User <givemeaccplz21@gmail.com>
2019-02-24 16:25:30 +0300

    com

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var err = {error:"You are rich, my friend! We don't have so much coins for exchange"};
+    if(currency < 1)
+    {
+        return {};
+    }
+    else if(currency >10000)
+    {
+       return err;
+    }
+    else{
+        var dict = {"H":50,"Q":25,"D":10,"N":5,"P":1};
+        console.log(dict.lenght);
+        for (const key in dict) {
+            {
+                var hqdnp = Math.floor(currency/dict[key]);
+                currency -= dict[key]*hqdnp;
+                dict[key] = hqdnp;
+                if(dict[key] == 0)
+                {
+                    delete dict[key];
+                }
+        }
+    }return dict;
 }
+}
+
